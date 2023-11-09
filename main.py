32e3c8d24deddbf32da2eca39889ad77e66bf16d
Eivind Teig <eivind.teig@gmail.com>
2023-11-09 11:25:37 +0100

    Add Biolerplate endpoints for frontend.

---
+++ main.py
@@ -0,0 +1,22 @@
+import fastapi
+import uvicorn
+from starlette.requests import Request
+from starlette.templating import Jinja2Templates
+
+api = fastapi.FastAPI()
+templates = Jinja2Templates('templates')
+
+@api.get('/')
+def index(request: Request):
+    return templates.TemplateResponse(
+        'home/index.html',
+        {'request': request}
+    )
+
+@api.get('/favicon.ico')
+def favicon():
+    return fastapi.responses.RedirectResponse(url='/static/img/favicon.ico')
+
+
+if __name__ == '__main__':
+    uvicorn.run(api, port=8000, host='127.0.0.1')
