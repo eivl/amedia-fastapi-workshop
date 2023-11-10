3a4fb9e0ef8ac6bf902e5d9e31cda15468277cb2
Eivind Teig <eivind.teig@gmail.com>
2023-11-10 09:07:03 +0100

    Refactor configurations.

--- main.py
+++ main.py
@@ -8,16 +8,23 @@ from views import home
 
 
 api = fastapi.FastAPI()
-api.include_router(home.router)
-api.include_router(weather_api.router)
-
-api.mount(
-    '/static',
-    StaticFiles(directory='static'),
-    name='static')
 
 
+def configure_routing():
+    api.include_router(home.router)
+    api.include_router(weather_api.router)
+    api.mount(
+        '/static',
+        StaticFiles(directory='static'),
+        name='static')
+
+
+def configure():
+    configure_routing()
 
 
 if __name__ == '__main__':
+    configure()
     uvicorn.run(api, port=8000, host='127.0.0.1')
+else:
+    configure()
