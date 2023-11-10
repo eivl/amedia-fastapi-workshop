eff61e026b7b554abeca75cd481d4b0171ede9d7
Eivind Teig <eivind.teig@gmail.com>
2023-11-10 10:18:17 +0100

    Add async GET functionality.

--- services/openweather_service.py
+++ services/openweather_service.py
@@ -1,12 +1,14 @@
 from typing import Optional
 from dotenv import load_dotenv
 import os
+import httpx
+
 
 load_dotenv()
 
 API_KEY = os.environ.get('OPEN_WEATHERMAP_API')
 
-def get_report(city: str,
+async def get_report(city: str,
                state: Optional[str],
                country: str,
                units: str):
@@ -17,4 +19,13 @@ def get_report(city: str,
 
 
     api_url = 'https://api.openweathermap.org/data/2.5/weather'
-    url = f'{api_url}?q={query}&appid={API_KEY}?units={units}'
+    url = f'{api_url}?q={query}&appid={API_KEY}&units={units}'
+
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url)
+        response.raise_for_status()
+
+    data = response.json()
+    forecast = data['main']
+
+    return forecast
