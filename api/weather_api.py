153f203e9cdef32cfb97dba5d3663a92812810bc
Eivind Teig <eivind.teig@gmail.com>
2023-11-10 14:59:18 +0100

    Add docstring and return type.

--- api/weather_api.py
+++ api/weather_api.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, Literal
 import fastapi
 from fastapi import Depends
 
@@ -10,7 +10,19 @@ router = fastapi.APIRouter()
 
 
 @router.get("/api/weather/{city}")
-async def weather(loc: Location = Depends(), units: Optional[str] = "metric"):
+async def weather(
+    loc: Location = Depends(),
+    units: Literal["metric", "standard", "imperial"] = "metric",
+) -> dict | fastapi.Response:
+    """
+    Get weather data for a city. Currently only one service provider is
+    implemented, hence the lack of a provider parameter. If more
+    providers are added, the provider parameter will be added and the
+    units literal needs to be changed.
+    :param loc: Data model for location.
+    :param units: "metric", "standard", "imperial"
+    :return: dict with weather data or fastapi.Response with error message.
+    """
     try:
         report = await openweather_service.get_report(
             loc.city, loc.state, loc.country, units
