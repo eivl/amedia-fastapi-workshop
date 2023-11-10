3bbbcf0ca9d4fcc6f894dfa69631263f3aa8022b
Eivind Teig <eivind.teig@gmail.com>
2023-11-10 14:29:12 +0100

    Add auth.user_management.

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ from auth.fake_user_db import fake_users_db
 from auth.password import verify_password, get_password_hash
 
 from api import weather_api
+from auth.user_management import authenticate_user, get_current_active_user
 from views import home
 
 from models.user import User, UserInDB
@@ -40,21 +41,6 @@ def configure():
 
 
 
-
-
-def get_user(db, username: str):
-    if username in db:
-        user_dict = db[username]
-        return UserInDB(**user_dict)
-
-def authenticate_user(fake_db, username: str, password: str):
-    user = get_user(fake_db, username)
-    if not user:
-        return False
-    if not verify_password(password, user.hashed_password):
-        return False
-    return user
-
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
     to_encode = data.copy()
     if expires_delta:
@@ -66,32 +52,6 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
     return encoded_jwt
 
 
-async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-        token_data = TokenData(username=username)
-    except JWTError:
-        raise credentials_exception
-    user = get_user(fake_users_db, username=token_data.username)
-    if user is None:
-        raise credentials_exception
-    return user
-
-
-async def get_current_active_user(
-    current_user: Annotated[User, Depends(get_current_user)]
-):
-    if current_user.disabled:
-        raise HTTPException(status_code=400, detail="Inactive user")
-    return current_user
 
 @api.post("/token", response_model=Token)
 async def login_for_access_token(
