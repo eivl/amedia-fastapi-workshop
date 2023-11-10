e5136e94b1352d1e2531a8d008ba7e75c58bc806
Eivind Teig <eivind.teig@gmail.com>
2023-11-10 14:37:56 +0100

    Clean up imports.

--- main.py
+++ main.py
@@ -1,27 +1,11 @@
-from datetime import datetime, timedelta
-from typing import Annotated
-
 import fastapi
 import uvicorn
-from jose import jwt, JWTError
-from passlib.context import CryptContext
-from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-
 
 from starlette.staticfiles import StaticFiles
 
-from auth.constants import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
-from auth.context import oauth2_scheme, pwd_context
-from auth.fake_user_db import fake_users_db
-from auth.password import verify_password, get_password_hash
-
 from api import weather_api
-from auth.user_management import authenticate_user, get_current_active_user
 from views import home
 
-from models.user import User, UserInDB
-from models.token import Token, TokenData
 
 api = fastapi.FastAPI()
 
